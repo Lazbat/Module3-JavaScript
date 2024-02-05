c2b6a2225c7d4c805c4f1d683944b26dc0e74c3f
Lazbat <babayalelazbat@gmail.com>
2024-02-05 22:06:05 +0100

    new commit

---
+++ Task13.js
@@ -0,0 +1,10 @@
+const books = {
+    title:'True Betrayal',
+    description:'Romance',
+    numberOfPages:345,
+    author:'Nora Roberts',
+    reading:true
+}
+
+console.log(books.description)
+console.log(books['author'])
